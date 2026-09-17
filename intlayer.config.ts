b0f9aa84129725eb4507845e89473a1f70de6284
aymericzip <ay.pineau@gmail.com>
2026-09-17 14:07:14 -0600

    chore: refactor internationalization locales array and add compiler output configuration

--- intlayer.config.ts
+++ intlayer.config.ts
@@ -2,12 +2,11 @@ import { Locales, type IntlayerConfig } from 'intlayer';
 
 const config: IntlayerConfig = {
   internationalization: {
-    locales: [
-      Locales.ENGLISH,
-      Locales.FRENCH,
-      Locales.SPANISH,
-    ],
+    locales: [Locales.ENGLISH, Locales.FRENCH, Locales.SPANISH],
     defaultLocale: Locales.ENGLISH,
+  },
+  compiler: {
+    output: ({ fileName }) => `./${fileName}.content.ts`,
   },
 };
 
